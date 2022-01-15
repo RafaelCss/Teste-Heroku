11e0b1239e3f251fbd23ad755cd5d410a64c0e5e
Rafael Carlos De Souza <rf2014souza@gmail.com>
2022-01-14 21:00:40 -0300

    Ajustes

--- src/Components/RouterApp.js
+++ src/Components/RouterApp.js
@@ -1,9 +1,21 @@
 import React from "react";
 import styled from 'styled-components';
 
-const App = styled.body`
+export const Body = styled.body`
 background-color: aqua;
 
 `
 
-export default App;
+export const App = ( ) => {
+  return (
+    <Body>
+      <>
+      <h1>Teste de Aplicação</h1>
+
+      <h3> Tudo certo !</h3>
+      </>
+    </Body>
+  )
+}
+
+
